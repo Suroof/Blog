5d7f87930be2799b2ef4d6abd35c4fbd75e5ed2c
Suroof <120703600+Suroof@users.noreply.github.com>
2024-12-11 22:38:46 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/articles" element={<Articles />} />
-              {/* <Route path="/projects" element={<Projects />} /> */}
+               <Route path="/projects" element={<Projects />} /> 
 
               {/* Article category routes */}
               <Route path="/articles/gaming" element={<Gaming />} />
