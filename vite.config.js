a3bff317527ff4c3ff6f08941ce6a766660b80cc
Suroof <120703600+Suroof@users.noreply.github.com>
2025-03-10 18:01:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+   build: {
+    rollupOptions: {
+      external: ['gsap'],
+    },
+  },
   publicDir: 'public'
 })
